da78648b0fd5fb1bfd511d06dd2d1baa0ddbf142
phai0512 <phai0512@gmail.com>
2017-12-16 06:42:27 +0900

    modify list data to rss

---
+++ NewsApp/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  NewsApp
+//
+//  Created by 小池基文 on 2017/12/14.
+//  Copyright © 2017年 小池基文. All rights reserved.
+//
+
+class Item {
+    var title = ""
+    var link = ""
+}
--- NewsApp/ListViewController.swift
+++ NewsApp/ListViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
-    
-    private let news = ["記事１", "記事２", "記事３"]
+class ListViewController: UITableViewController, XMLParserDelegate {
+
+    var perser:XMLParser!
+    var items = [Item]()
+    var item:Item?
+    var currentString = ""
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return news.count
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel!.text = news[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row].title
         return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startDownload()
+    }
+    
+    func startDownload() {
+        self.items = []
+        if let url = URL(string: "http://www.apple.com/jp/pr/feeds/pr.rss") {
+            if let perser = XMLParser(contentsOf: url) {
+                self.perser = perser
+                self.perser.delegate = self
+                self.perser.parse()
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        self.currentString = ""
+        if elementName == "item" {
+            self.item = Item()
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        self.currentString += string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch elementName {
+        case "title": self.item?.title = currentString
+        case "link": self.item?.link = currentString
+        case "item": self.items.append(self.item!)
+        default: break
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        self.tableView.reloadData()
     }
 }
